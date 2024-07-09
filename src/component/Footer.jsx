af2a529250c0e0b89efe9a24828ae12f6a4fa341
Ogun6ele <kehindegundel@gmail.com>
2024-07-09 12:21:30 +0100

    5th Commit

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,8 +1,8 @@
 
 const Footer = () => {
   return (
-    <footer className=" flex flex-col  justify-center px-[50px] gap-10 md:flex-row md:justify-between md:px-[100px] py-[40px] w-full bg-footer">
-        <div className="flex justify-center m-auto items-left flex-col">
+    <footer className=" flex flex-col  justify-left m-auto px-[50px] gap-10 md:flex-row md:justify-between md:px-[100px] py-[40px] w-full bg-footer">
+        <div className="flex justify-center items-left flex-col">
             <h2>About Us</h2>
             <ul className='flex mt-3  flex-col gap-3'>
                 <li><a className="hover:underline">Contact Us</a></li>
@@ -10,7 +10,7 @@ const Footer = () => {
                 <li><a className="hover:underline">Terms & Conditions</a></li>
             </ul>
         </div>
-        <div className="flex justify-center m-auto items-left flex-col">
+        <div className="flex justify-center items-left flex-col">
             <h2>Payment</h2>
             <ul className='flex  mt-3  flex-col gap-3'>
                 <li><a className="hover:underline">Opay</a></li>
@@ -18,7 +18,7 @@ const Footer = () => {
                 <li><a className="hover:underline">Kuda</a></li>
             </ul>
         </div>
-        <div className="flex justify-center m-auto items-left flex-col">
+        <div className="flex justify-center items-left flex-col">
             <h2>Info</h2>
             <ul className='flex  mt-3  flex-col gap-3'>
                 <li><a className="hover:underline">FAQs</a></li>
